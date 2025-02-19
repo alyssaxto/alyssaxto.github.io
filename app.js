f47a8caeb23f46317e71019d5e5585afdad19bc8
Alyssa To <142935267+alyssaxto@users.noreply.github.com>
2025-02-19 14:15:08 -0600

    fade

--- app.js
+++ app.js
@@ -82,6 +82,13 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileMenuButton.classList.toggle('active');
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".main__content").classList.add("show");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".main__content").classList.add("show");
+});
 
 
 document.addEventListener("DOMContentLoaded", function () {
